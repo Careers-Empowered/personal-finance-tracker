d1ad06e736e8bb14016660cd778693504ed0d281
Monica <monica.2301152@srec.ac.in>
2026-08-11 21:52:52 +0530

    Initial setup for home page

---
+++ frontend/src/features/home/Home.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Home: React.FC = () => {
+  return (
+    <div className="page-container">
+      <h1 className="page-title">Personal Finance</h1>
+      <div className="page-card">
+        <p>Welcome to your Personal Finance Tracker. </p>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
---
+++ frontend/src/features/scheduled/Scheduled.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Scheduled: React.FC = () => {
+  return <div className="page-container" />;
+};
+
+export default Scheduled;
